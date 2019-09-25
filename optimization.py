8ba8061db1716c74a7bf6914a212e561c2603747
42ea <erian@axis.com>
2019-09-25 12:47:21 +0200

    Created the compute_gradient method

--- optimization.py
+++ optimization.py
@@ -13,9 +13,17 @@ class Optimization(object):
             self.gradient = gradient
     
     @methodclass
-    def optimize(cls,opt_object,methods)
-
-    def compute_gradient(self,f,x)
+    def optimize(cls,opt_object,methods):
+        
+    def compute_gradient(self,x_k,x_km1):
+        if self.gradient != None:
+            return self.gradient(x_k)
+        gradient_k = np.zeros(1,len(x_k))
+        delta_x0 = x_k[0]-x_km1[0] 
+        delta_x1 = x_k[1]-x_km1[1]
+        x0 = np.array([delta_x0, x_k[1]])
+        x1 = np.array([x_k[0], delta_x1])
+        return np.array([self.objective_function(x0)/delta_x0, self.objective_function(x1)/delta_x1])
 
 
 
