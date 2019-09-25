d4280c99fa37d38fc5334bab7183a0e325f77dce
42ea <erian@axis.com>
2019-09-25 12:28:18 +0200

    Started on framework foundations

--- optimization.py
+++ optimization.py
@@ -2,6 +2,23 @@
     Script containing an optimization problem class, which contains methods for
     solving optimization problems using different methods
                                                                             '''
-                                                                            
+import numpy as np
+import scipy
+
 class Optimization(object):
+
+    def __init__(self,objective_function,gradient=None):
+        self.objective_function = objective_function
+        if gradient != None:
+            self.gradient = gradient
+    
+    @methodclass
+    def optimize(cls,opt_object,methods)
+
+    def compute_gradient(self,f,x)
+
+
+
+
+
     
